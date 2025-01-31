c49a33a27d64639b85795df56532df8735260efa
mustafagadi <mustafagadi10@gmail.com>
2025-01-31 22:07:07 +0300

    Add files via upload

--- ReceiveMails/Program.cs
+++ ReceiveMails/Program.cs
@@ -1,3 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Kartoum");
-Console.ReadLine();
+﻿using System;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Xml;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string imapHost = "imap.gmail.com";
+        int imapPort = 993;
+        string emailAddress = "your-email@gmail.com";
+        string emailPassword = "your-app-password";
+
+        var imapService = new ImapEmailService(imapHost, imapPort, emailAddress, emailPassword);
+
+        Console.WriteLine("Starting email listener...");
+        Console.WriteLine("Press 'q' to stop the listener.");
+
+        _ = Task.Run(() => imapService.StartListeningAsync());
+
+        while (Console.ReadKey(true).Key != ConsoleKey.Q) { }
+
+        await imapService.StopListeningAsync();
+
+        Console.WriteLine("Email listener stopped.");
+    }
+}
+
+
